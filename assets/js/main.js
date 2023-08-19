23e72c4a692acb852ed7b5e3132bfe87117c585c
Kauan Magaldi <kauan@MacBook-Pro-de-Kauan.local>
2023-08-19 17:20:18 -0300

    oi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Scaffold
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
